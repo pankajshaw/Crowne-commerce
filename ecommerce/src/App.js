5b9b556caee29725637edcf3e14cacc6467d8f23
pankajshaw <shawpankaj30@gmail.com>
2019-08-27 00:42:46 +0530

    Updated with Shop Page , collection Preview component and collection Item Component

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
