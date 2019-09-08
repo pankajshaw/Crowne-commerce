d6e4993b29f99052e11bd8067497d4920c77914b
pankajshaw <shawpankaj30@gmail.com>
2019-09-08 19:37:27 +0530

    Firestore , Add the data to the firebase(database) and to the state , sign-up component with styles , sign-in with email and password

--- ecommerce/src/components/sign-in/sign-in.component.jsx
+++ ecommerce/src/components/sign-in/sign-in.component.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
-import "./sign-in.styles.scss";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
+
+import "./sign-in.styles.scss";
 
 class SignIn extends React.Component {
   constructor(props) {
@@ -14,33 +16,39 @@ class SignIn extends React.Component {
       password: ""
     };
   }
-  handleSubmit = event => {
-    event.preventDefault();
 
-    this.setState({ email: "", password: "" });
+  handleSubmit = async event => {
+    event.preventDefault();
+    const { email, password } = this.state;
+    try {
+      await auth.createUserWithEmailAndPassword(email, password);
+      this.setState({ email: "", password: "" });
+    } catch (error) {
+      console.log(error.message);
+    }
   };
 
   handleChange = event => {
-    const { name, value } = event.target;
+    const { value, name } = event.target;
 
     this.setState({ [name]: value });
   };
+
   render() {
     return (
       <div className="sign-in">
-        <h2 className="title">I already have an account</h2>
-        <span>Sign in with your Email and Password </span>
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password</span>
 
         <form onSubmit={this.handleSubmit}>
           <FormInput
             name="email"
             type="email"
-            value={this.state.email}
             handleChange={this.handleChange}
+            value={this.state.email}
             label="email"
             required
           />
-
           <FormInput
             name="password"
             type="password"
@@ -50,9 +58,9 @@ class SignIn extends React.Component {
             required
           />
           <div className="buttons">
-            <CustomButton type="submit">Sign In</CustomButton>
+            <CustomButton type="submit"> Sign in </CustomButton>
             <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
-              Sign In With Google
+              Sign in with Google
             </CustomButton>
           </div>
         </form>
--- ecommerce/src/firebase/firebase.utils.js
+++ ecommerce/src/firebase/firebase.utils.js
@@ -1,5 +1,4 @@
 import firebase from "firebase/app";
-
 import "firebase/firestore";
 import "firebase/auth";
 
@@ -8,12 +7,36 @@ const config = {
   authDomain: "crown-db-16670.firebaseapp.com",
   databaseURL: "https://crown-db-16670.firebaseio.com",
   projectId: "crown-db-16670",
-  storageBucket: "",
+  storageBucket: "crown-db-16670.appspot.com",
   messagingSenderId: "579302720603",
-  appId: "1:579302720603:web:d93c6a2c04cc0ed5"
+  appId: "1:579302720603:web:0495c9b2189d80fe"
 };
 
 firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log("error creating User", error.message);
+    }
+  }
+  return userRef;
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
